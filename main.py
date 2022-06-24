502e7e41529f38aed7bead70b3942029a99a7ade
ihaveseeni486 <my.vhw.mail@gmail.com>
2022-06-24 17:24:00 +0500

    Добавлен выбор режима слияния

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     contacts_dict_result = {}
     contacts_list_result = []
 
+    clone_flag = int(input("Выберите режим объединения дублей. 1 - полное совпадение ФИО, 2 - частичное совпадение ФИ: "))
+
     with open("phonebook_raw.csv") as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
@@ -26,13 +28,25 @@ if __name__ == '__main__':
             str_fio_splitted = re.split(pattern, str_fio)
 
             if len(str_fio_splitted) > 1:
-                str_fio = ' '.join(str(x) for x in str_fio_splitted)
-                if str_fio in contacts_dict_result:
+                str_fio = ' '.join(str(x) for x in str_fio_splitted if x not in ("", None))
+                found_clone = False
+                if clone_flag == 1:
+                    if str_fio in contacts_dict_result:
+                        found_clone = True
+                else:
+                    filtered_dict_fi = dict(filter(lambda x: x[0].startswith(str_fio) or str_fio.startswith(x[0]),
+                                                   contacts_dict_result.items()))
+
+                    if len(filtered_dict_fi) > 0:
+                        found_clone = True
+                        str_fio = str(list(filtered_dict_fi.keys())[0])
+                if found_clone:
                     # если по фио совпадет уже человек, то обновим данные
                     info_old = contacts_dict_result[str_fio]
                     info_old[0] = str_fio_splitted[1]
                     info_old[1] = str_fio_splitted[2]
-                    info_old[2] = str_fio_splitted[3]
+                    if str_fio_splitted[3] not in ("", None):
+                        info_old[2] = str_fio_splitted[3]
                     if contact[3] != "":
                         info_old[3] = contact[3]
                     if contact[4] != "":
